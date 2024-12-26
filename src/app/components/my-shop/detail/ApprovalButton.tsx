9750584bacced490494e9aeff44099eb634ee553
JUYOUNG\vilio <vilioite@naver.com>
2024-12-27 06:13:24 +0900

    fix: 공고 상세 페이지네이션 버그 수정

--- src/app/components/my-shop/detail/ApprovalButton.tsx
+++ src/app/components/my-shop/detail/ApprovalButton.tsx
@@ -1,3 +1,6 @@
+import NoticeRegisterModal from '@/app/components/modal/NoticeRegisterModal';
+import useModalStore from '@/app/stores/modalStore';
+
 export default function ApprovalButton({
   approve,
   onClick,
@@ -5,21 +8,27 @@ export default function ApprovalButton({
   approve: boolean;
   onClick: () => Promise<void>;
 }) {
+  const { openModal, isModalOpen } = useModalStore();
+
   const isApprove = approve
-    ? { content: '승인하기', style: 'border-blue-20 text-blue-20' }
-    : { content: '거절하기', style: 'border-red-40 text-red-40' };
+    ? { content: '승인하기', style: 'border-blue-20 text-blue-20', modal: '신청을 승인하시겠어요?' }
+    : {
+        content: '거절하기',
+        style: 'border-red-40 text-red-40',
+        modal: '신청을 거절하시겠어요?',
+      };
 
-  const handleOnClick = async () => {
-    const isConfirmed = window.confirm('신청을 처리하시겠습니까?');
-
-    if (isConfirmed) {
-      await onClick();
-    }
+  const handleOnClick = () => {
+    if (isModalOpen) return;
+    openModal();
   };
 
   return (
-    <button className={`rounded-lg border px-5 py-2 ${isApprove.style}`} onClick={handleOnClick}>
-      {isApprove.content}
-    </button>
+    <>
+      <button className={`rounded-lg border px-5 py-2 ${isApprove.style}`} onClick={handleOnClick}>
+        {isApprove.content}
+      </button>
+      {isModalOpen && <NoticeRegisterModal content={isApprove.modal} onClick={onClick} />}
+    </>
   );
 }
